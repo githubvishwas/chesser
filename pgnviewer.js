a480050b81e04c057fd6c4670d5354b73835406f
githubvishwas <vishwas_nitk@rediffmail.com>
2018-11-18 08:16:17 -0800

    stockfish updates

--- pgnviewer.js
+++ pgnviewer.js
@@ -9,8 +9,8 @@ var onDragStart = function(source, piece, position, orientation) {
 };
 
 var onDrop = function(source, target) {
-
-	
+  //in pgn viewer we dont want any user movements
+   return 0;	
   // see if the move is legal
   var move = game.move({
     from: source,
@@ -261,7 +261,11 @@ $('#nextBtn5').on('click', function() {
   });
   // 5. If Start button clicked, go to start position
   $('#startPositionBtn5').on('click', function() {
-    reset()
+	for(var i = istep; i > 0; i--) {
+        game.undo();
+    }
+	
+    board.position(game.fen());
     istep = 0;
   });
 
@@ -282,8 +286,19 @@ $('#nextBtn5').on('click', function() {
     for (var i = 0, f; f = files[i]; i++) {
 			var reader = new FileReader();
 			reader.onload = function(e) {
-			  pgnText = reader.result;
-			  statusEl.html(pgnText);
+			pgnText = reader.result;
+			game.load_pgn(pgnText);
+			board.position(game.fen())
+			  
+			var lsol = game.history();
+		
+			solArray = lsol.toString().split(',');
+			istep = solArray.length;
+			create_table();
+			AnalyzePGN();
+			console.log("move_score_array: ",move_score_array);
+			console.log("solArray: ", solArray)
+			  //statusEl.html(pgnText);
 			}
 	reader.readAsText(f);
 
@@ -297,7 +312,44 @@ $('#nextBtn5').on('click', function() {
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
   }
 
-  
+function create_table()
+    {
+       
+
+		var myTable= "<table><tr><td style='width: 100px; color: red;'>White move</td>";
+		myTable+= "<td style='width: 100px; color: red; text-align: right;'>CP eval</td>";
+		myTable+="<td style='width: 100px; color: red; text-align: right;'>Black move</td>";
+		myTable+="<td style='width: 100px; color: red; text-align: right;'>CP eval</td></tr>";
+
+		myTable+="<tr><td style='width: 100px;                   '>---------------</td>";
+		myTable+="<td     style='width: 100px; text-align: right;'>---------------</td>";
+		myTable+="<td     style='width: 100px; text-align: right;'>---------------</td>";
+		myTable+="<td     style='width: 100px; text-align: right;'>---------------</td></tr>";
+		
+		var num_rows = Math.floor(solArray.length/2);
+		var remainder = solArray.length % 2;
+		if(remainder !== 0) {
+			num_rows++;
+		}
+		var cntr = 0
+	    for (var i=0; i<num_rows; i++) {
+		  myTable+="<tr id=i.toString()><td id=cntr.toString() style='width: 100px;text-align: right;'>" + solArray[cntr] + "</td>";
+		  cntr++
+		  myTable+="<td id=cntr.toString() style='width: 100px; text-align: right;'>0.0</td>";
+		  var move = ""
+		  if(cntr < solArray.length) {
+			move = solArray[cntr]
+		  }
+		  myTable+="<td id=cntr.toString() style='width: 100px; text-align: right;'>" + move + "</td>";
+		  myTable+="<td id=cntr.toString() style='width: 100px; text-align: right;'>0.0</td></tr>";
+		  cntr++
+	    }  
+	   myTable+="</table>";
+	   document.getElementById('tablePrint').innerHTML = myTable;
+	    //var Row = document.getElementById(0);
+		//var Cells = Row.getElementById(0);
+		//alert(Cells[0].innerText);
+    }  
 function main() {
 	
 	var cfg = {
@@ -315,9 +367,255 @@ function main() {
 
 	updateStatus();
 }
+
+//Stockfish code below
+
+function error(str)
+{
+	str = str || "Unknown error";
+	
+	alert("An error occured.\n" + str);
+	throw new Error(str);
+}
+
+function load_engine()
+{
+	var worker = new Worker("js/stockfish6.js"),
+		engine = {started: Date.now()},
+		que = [];
+	
+	function get_first_word(line)
+	{
+		var space_index = line.indexOf(" ");
+		
+		/// If there are no spaces, send the whole line.
+		if (space_index === -1) {
+			return line;
+		}
+		return line.substr(0, space_index);
+	}
+	
+	function determine_que_num(line, que)
+	{
+		var cmd_type,
+			first_word = get_first_word(line),
+			cmd_first_word,
+			i,
+			len;
+		
+		if (first_word === "uciok" || first_word === "option") {
+			cmd_type = "uci"
+		} else if (first_word === "readyok") {
+			cmd_type = "isready";
+		} else if (first_word === "bestmove" || first_word === "info") {
+			cmd_type = "go";
+		} else {
+			/// eval and d are more difficult.
+			cmd_type = "other";
+		}
+		
+		len = que.length;
+		
+		for (i = 0; i < len; i += 1) {
+			cmd_first_word = get_first_word(que[i].cmd);
+			if (cmd_first_word === cmd_type || (cmd_type === "other" && (cmd_first_word === "d" || cmd_first_word === "eval"))) {
+				return i;
+			}
+		}
+		
+		/// Not sure; just go with the first one.
+		return 0;
+	}
+	
+	worker.onmessage = function (e)
+	{
+		var line = e.data,
+			done,
+			que_num = 0,
+			my_que;
+		
+		/// Stream everything to this, even invalid lines.
+		if (engine.stream) {
+			engine.stream(line);
+		}
+		
+		/// Ignore invalid setoption commands since valid ones do not repond.
+		if (line.substr(0, 14) === "No such option") {
+			return;
+		}
+		
+		que_num = determine_que_num(line, que);
+		
+		my_que = que[que_num];
+		
+		if (!my_que) {
+			return;
+		}
+		
+		if (my_que.stream) {
+			my_que.stream(line);
+		}
+		
+		if (typeof my_que.message === "undefined") {
+			my_que.message = "";
+		} else if (my_que.message !== "") {
+			my_que.message += "\n";
+		}
+		
+		my_que.message += line;
+		
+		/// Try to determine if the stream is done.
+		if (line === "uciok") {
+			/// uci
+			done = true;
+			engine.loaded = true;
+		} else if (line === "readyok") {
+			/// isready
+			done = true;
+			engine.ready = true;
+		} else if (line.substr(0, 8) === "bestmove") {
+			/// go [...]
+			done = true;
+			/// All "go" needs is the last line (use stream to get more)
+			my_que.message = line;
+		} else if (my_que.cmd === "d" && line.substr(0, 15) === "Legal uci moves") {
+			done = true;
+		} else if (my_que.cmd === "eval" && /Total Evaluation[\s\S]+\n$/.test(my_que.message)) {
+			done = true;
+		} else if (line.substr(0, 15) === "Unknown command") {
+			done = true;
+		}
+		///NOTE: Stockfish.js does not support the "debug" or "register" commands.
+		///TODO: Add support for "perft", "bench", and "key" commands.
+		///TODO: Get welcome message so that it does not get caught with other messages.
+		///TODO: Prevent (or handle) multiple messages from different commands
+		///      E.g., "go depth 20" followed later by "uci"
+		
+		if (done) {
+			if (my_que.cb && !my_que.discard) {
+				my_que.cb(my_que.message);
+			}
+			
+			/// Remove this from the que.
+			//G.array_remove(que, que_num);
+		}
+	};
+	
+	engine.send = function send(cmd, cb, stream)
+	{
+		cmd = String(cmd).trim();
+		
+		/// Can't quit. This is a browser.
+		///TODO: Destroy the engine.
+		if (cmd === "quit") {
+			return;
+		}
+		
+		if (debugging) {
+			console.log("1: " + cmd);
+		}
+		
+		/// Only add a que for commands that always print.
+		///NOTE: setoption may or may not print a statement.
+		if (cmd !== "ucinewgame" && cmd !== "flip" && cmd !== "stop" && cmd !== "ponderhit" && cmd.substr(0, 8) !== "position"  && cmd.substr(0, 9) !== "setoption") {
+			que[que.length] = {
+				cmd: cmd,
+				cb: cb,
+				stream: stream
+			};
+		}
+		worker.postMessage(cmd);
+	};
+	
+	engine.stop_moves = function stop_moves()
+	{
+		var i,
+			len = que.length;
+		
+		for (i = 0; i < len; i += 1) {
+			if (debugging) {
+				console.log(i, get_first_word(que[i].cmd))
+			}
+			/// We found a move that has not been stopped yet.
+			if (get_first_word(que[i].cmd) === "go" && !que[i].discard) {
+				engine.send("stop");
+				que[i].discard = true;
+			}
+		}
+	}
+	
+	engine.get_cue_len = function get_cue_len()
+	{
+		return que.length;
+	}
+	
+	return engine;
+}
+
+function AnalyzePGN()
+{
+	console.log("Init")	
+	if (typeof Worker === "undefined") {
+		return alert("Sorry, analyzer does not support this browser.");
+	}
+	
+	for(var i = istep; i > 0; i--) {
+        game.undo();
+    }
+	
+    board.position(game.fen());
+    istep = 0;
+	evaler = load_engine();
+	evaler.send("uci");
+	evaler.send("ucinewgame");
+	for(var i = istep; i < solArray.length; i++) {
+        game.move(solArray[i]);
+		
+		evaler.send("position fen r1k4r/p2nb1p1/2b4p/1p1n1p2/2PP4/3Q1NB1/1P3PPP/R5K1 b - c3 0 19");
+		//evaler.send("go movetime 1000");
+		evaler.send("go movetime 1000", function ongo(str)
+		{
+			console.log("Calculating")	
+			var matches = str.match(/^bestmove\s(\S+)(?:\sponder\s(\S+))?/);
+			
+			evaler.busy = false;
+			move_score_array.push(move_score);
+			//G.events.trigger("evaled", {ply: ply});
+		}, function stream(str)
+		{
+			console.log("str: ", str)
+			var matches = str.match(/depth (\d+) .*score (cp|mate) ([-\d]+) .*pv (.+)/),
+				score,
+				type,
+				depth,
+				pv,
+				data;
+			 if (matches) {
+					depth = Number(matches[1]);
+					type = matches[2];
+					score = Number(matches[3]);
+					pv = matches[4].split(" ");
+					move_score = score;
+					console.log("Depth: ", depth)	
+					console.log("type: ", type)	
+					console.log("score: ", score)	
+					console.log("pv: ", pv)	
+			} else {
+				statusEl.html("Score: No matches");	
+			}			
+			
+		});
+    }
+    
+    board.position(game.fen());
+    istep = solArray.length;
+	
+	
+	
+}
 var board
 var game = new Chess()
-var statusEl = $('#status');
+var statusEl = $('#pgn5');
 var startPos = ""
 var solpgn = ""
 var solArray = ""
@@ -330,5 +628,10 @@ var toplay = ""
 var g_orientation = ""
 var istep = 0;
 var pgnText = ""
+var layout = {};
+var evaler;
+var debugging = true;
+var move_score = 0.0;
+var move_score_array = ""
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 main()
